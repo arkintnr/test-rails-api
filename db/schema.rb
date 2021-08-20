d131652ecc687c962e1618862a2ea8f2ab1332c7
vncnt <vjkcalag@gmail.com>
2021-08-20 21:00:17 +0800

    init

---
+++ db/schema.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20_210_820_141_816) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
+  create_table 'guests', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.text 'phone', default: [], array: true
+    t.string 'email', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_guests_on_email', unique: true
+  end
+
+  create_table 'reservations', force: :cascade do |t|
+    t.datetime 'start_date', null: false
+    t.datetime 'end_date', null: false
+    t.string 'currency', default: 'USD', null: false
+    t.decimal 'payout_price', precision: 10, scale: 2, default: '0.0', null: false
+    t.decimal 'security_price', precision: 10, scale: 2, default: '0.0', null: false
+    t.decimal 'total_price', precision: 10, scale: 2, default: '0.0', null: false
+    t.integer 'nights', default: 0, null: false
+    t.integer 'guests', default: 0, null: false
+    t.integer 'adults', default: 0, null: false
+    t.integer 'children', default: 0, null: false
+    t.integer 'infants', default: 0, null: false
+    t.string 'status', default: '', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'guest_id', null: false
+    t.index ['guest_id'], name: 'index_reservations_on_guest_id'
+  end
+
+  add_foreign_key 'reservations', 'guests'
+end
